702c089e3eb941a1b054bccb8a36cd1ab981dce0
CJ484 <ccoreas484@gmail.com>
2023-11-04 19:45:10 -0400

    start base for website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './utils/i18Next/index'
+import './utils/i18n/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
