2eb7759a0425901e002b2bfc71e22f9d37606693
CJ484 <ccoreas484@gmail.com>
2023-11-09 14:07:45 -0500

    skill svg uploaded

---
+++ src/assets/skills/index.js
@@ -0,0 +1,29 @@
+import aws_api_gateway from "./aws-api-gateway.svg";
+import aws_lambda from "./aws-lambda.svg";
+import aws_ses from "./aws-ses.svg";
+import bootstrap from "./bootstrap.svg";
+import css from "./css.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+import jest from "./jest.svg";
+import material_ui from "./material-ui.svg";
+import netlify from "./netlify.svg";
+import react from "./react.svg";
+import redux_saga from "./redux-saga.svg";
+import scss from "./scss.svg";
+
+export {
+  aws_api_gateway,
+  aws_lambda,
+  aws_ses,
+  bootstrap,
+  css,
+  html,
+  javascript,
+  jest,
+  material_ui,
+  netlify,
+  react,
+  redux_saga,
+  scss,
+};
