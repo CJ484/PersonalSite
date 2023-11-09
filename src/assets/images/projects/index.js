8aef97799707a5baa725070ed8546a969867d458
CJ484 <ccoreas484@gmail.com>
2023-11-09 14:31:28 -0500

    Add project images to assets.

---
+++ src/assets/images/projects/index.js
@@ -0,0 +1,8 @@
+import connect_four from './connect_four.png';
+import dogwalk from './dogwalk.png';
+import final_project from './final_project.png';
+import pokedex from './pokedex.png';
+import prestige from './prestige.png';
+
+
+export { connect_four, dogwalk, final_project, pokedex, prestige };
