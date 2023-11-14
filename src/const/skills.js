538d4b92056d5e1627dc7a145e30c6867b92de5e
CJ484 <ccoreas484@gmail.com>
2023-11-13 20:10:03 -0500

    Add skills constants to project

---
+++ src/const/skills.js
@@ -0,0 +1,76 @@
+import {
+  aws_api_gateway,
+  aws_lambda,
+  aws_ses,
+  bootstrap,
+  css,
+  html,
+  javascript,
+  jest,
+  material_ui,
+  netlify,
+  react,
+  redux_saga,
+  scss,
+} from "../assets/skills";
+
+const mySkills = {
+    skill_1: {
+        name: 'HTML',
+        image: html,
+    },
+    skill_2: {
+        name: 'CSS',
+        image: css,
+    },
+    skill_3: {
+        name: 'JavaScript',
+        image: javascript,
+    },
+    skill_4: {
+        name: 'React',
+        image: react,
+    },
+    skill_5: {
+        name: 'Redux',
+        image: react,
+    },
+    skill_6: {
+        name: 'Redux Saga',
+        image: redux_saga,
+    },
+    skill_7: {
+        name: 'Material UI',
+        image: material_ui,
+    },
+    skill_8: {
+        name: 'Bootstrap',
+        image: bootstrap,
+    },
+    skill_9: {
+        name: 'SASS',
+        image: scss,
+    },
+    skill_10: {
+        name: 'Jest',
+        image: jest,
+    },
+    skill_11: {
+        name: 'AWS Lambda',
+        image: aws_lambda,
+    },
+    skill_12: {
+        name: 'AWS SES',
+        image: aws_ses,
+    },
+    skill_13: {
+        name: 'AWS API Gateway',
+        image: aws_api_gateway,
+    },
+    skill_14: {
+        name: 'Netlify',
+        image: netlify,
+    },
+}
+
+export default mySkills;
