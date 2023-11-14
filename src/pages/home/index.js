4032f41ee93677728dc15d11988ab1d42cbeb074
CJ484 <ccoreas484@gmail.com>
2023-11-14 10:56:08 -0500

    Add skills, social media links, and technologies to home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,81 @@
-import avatar from '../../assets/images/myImage.jpg';
-import '../../styles/pages.scss';
+import mySkills from "../../const/skills";
+import avatar from "../../assets/images/myImage.jpg";
+import "../../styles/pages.scss";
+import github from "../../assets/symbols/github.svg";
+import linkedIn from "../../assets/symbols/linkedin.svg";
+import mail from "../../assets/symbols/mail.svg";
 
 const HomePage = () => {
   return (
-    <div className='home router-page'>
-      <img src={avatar} alt="avatar" />
-      <h1>Hi I'm Carlos</h1>
-      <h1>I am a Front-End Developer</h1>
+    <div className="home router-page light-theme">
+      <img className="myImg" src={avatar} alt="avatar" />
+
+      <h1>Hi, I'm <b>Carlos Coreas</b> & I am a</h1>
+      <h1 className="role">Front-End Developer 👨‍💻</h1>
+      <h3 className="subtitle">About me</h3>
+      <div className="homeSection">
+        <p>
+          I have experience in React and Agile
+          Methodologies. I got into coding watching movies and shows like
+          for some of you who remembers "Code Lyoko" or "The Core".
+          Every job I have received they would call me the IT guy because I
+          would always fix the computers and printers. I have always been
+          interested in technology and how it works. I am a very curious person
+          and love to learn new things. I am a very hard worker and I
+          am always looking for new challenges. I am on the verge of looking for
+          a new job opportunity that allows me to grow professionally and
+          personally.
+        </p>
+      </div>
+      <div className="homeSection">
+        <h5>👀 Currently looking for employment</h5>
+        <h5>📍 Currently located on Long Island, NY</h5>
+        <h5>📚 Learning to be a Full-Stack Developer & Web 3.0</h5>
+      </div>
+      <h3 className="subtitle">Social Media</h3>
+      <div className="social-media homeSection">
+        <a
+          className="social-link"
+          href="https://www.linkedin.com/in/carlos-coreas-247048101/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedIn} alt="linkedin symbol" />
+          <h5>@Carlos-Coreas</h5>
+        </a>
+        <a
+          className="social-link"
+          href="https://github.com/CJ484"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={github} alt="github symbol" />
+          <h5>@CJ484</h5>
+        </a>
+        <a
+          className="social-link"
+          href="ccoreas484@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={mail} alt="email symbol" />
+          <h5>@ccoreas484</h5>
+        </a>
+      </div>
+      <h3 className="subtitle">Technologies I use</h3>
+      <div className="homeSection">
+        <div className="skills">
+          {Object.keys(mySkills).map((key) => {
+            const { name, image } = mySkills[key];
+            return (
+              <div className="skill" key={key}>
+                <img src={image} alt={name} />
+                <h6>{name}</h6>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
